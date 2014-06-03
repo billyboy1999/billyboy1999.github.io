266afec1d42a19dc475ef40bddf373512695bf4a
John Jagg <willcthefourth@gmail.com>
2014-06-03 18:55:34 -0400

    images

--- javascript/rock_paper_scissors.js
+++ javascript/rock_paper_scissors.js
@@ -4,6 +4,8 @@ function rps() {
     if (computerChoice <= 0.33) {computerChoice="R";}
     else if (computerChoice <= 0.66) {computerChoice="P";}
     else {computerChoice="S";}
+	document.getElementById("pc").src = "pictures/".concat(userChoice.value, ".png")
+	document.getElementById("cc").src = "pictures/".concat(computerChoice, ".png")
     console.log(userChoice.value, computerChoice);
     document.getElementById("output").innerHTML = compare(userChoice.value, computerChoice)};
 
